a67ab378afd66ec2d07bbf9c0e9ef3fea894374f
JiayiH74 <jiayih12@nycstudents.net>
2024-09-25 11:18:02 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Feeder f = new Feeder();
+        System.out.println(f.getCurrentFood());
+    }
+    //System.out.println(f.currentFood)
+
+    
+
+    Feeder g = new Feeder(500);
+
+}
+

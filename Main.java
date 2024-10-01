4646caa1dc6af94076d1a099884730885643b525
JiayiH74 <jiayih12@nycstudents.net>
2024-10-01 11:27:00 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
-public class Main
+public class Main 
 {
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {
         Feeder f = new Feeder();
         System.out.println(f.getCurrentFood());
+        // System.out.println(f.currentFood)
+      
+       Feeder g = new Feeder(500);
+        System.out.println(g.getCurrentFood());
+
+        g.simulateOneDay(12);
+        System.out.println(g.getCurrentFood());
+
+        //amount of food eaten by each bird
+        int random = (int) (Math.random() * 41 + 10); 
+        System.out.println(random);
+
+        //5% chance of bear attack
+        System.out.println(Math.random() < 0.05);
+
+        Feeder h = new Feeder(1000);
+        h.simulateOneDay(22);
+        System.out.println(h.getCurrentFood());
+
+        Feeder i = new Feeder(100);
+        i.simulateOneDay(5);
+        System.out.println(i);
     }
-    //System.out.println(f.currentFood)
-
-    
-
-    Feeder g = new Feeder(500);
-
+   
 }
-
